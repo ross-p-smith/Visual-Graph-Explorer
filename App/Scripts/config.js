6a055bdd32373ba947950f4f6474865fb4d51ee9
ross-p-smith <ross-p-smith@users.noreply.github.com>
2016-10-28 14:32:32 +0100

    Update config.js

--- App/Scripts/config.js
+++ App/Scripts/config.js
@@ -1,5 +1,5 @@
 
-var PRODUCTION_URL = "visualgraphexplorer\.azurewebsites\.net";
+var PRODUCTION_URL = "fd2016graph\.azurewebsites\.net";
 var clientId = null;
 var azureADConfig = {};
 azureADConfig.endpoints = {
@@ -8,7 +8,7 @@ azureADConfig.endpoints = {
 };
 
 if (RegExp(PRODUCTION_URL, 'i').test(window.location.host)) {
-    azureADConfig.clientId = '78d5a9a4-c418-4199-ac9b-a7e66a61ad91';
+    azureADConfig.clientId = '65885396-cb45-4137-9c2c-b0a7099dce42';
 } else {
     azureADConfig.clientId = '509bde6e-7c29-4898-a829-2ecfea9dc241'; //this is the dev client id
 }
